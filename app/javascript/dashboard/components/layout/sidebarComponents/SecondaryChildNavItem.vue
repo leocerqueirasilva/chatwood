92164aed6d152d40edeb4de3593a6ae29ef65a5d
leocerqueirasilva <120220664+leocerqueirasilva@users.noreply.github.com>
2024-08-09 18:30:22 -0300

    Commit

--- app/javascript/dashboard/components/layout/sidebarComponents/SecondaryChildNavItem.vue
+++ app/javascript/dashboard/components/layout/sidebarComponents/SecondaryChildNavItem.vue
@@ -6,7 +6,7 @@
     active-class="active"
   >
     <li
-      class="font-medium h-7 my-1 hover:bg-slate-25 hover:text-bg-50 flex items-center px-2 rounded-md dark:hover:bg-slate-800"
+      class="font-medium h-7 my-1 hover:bg-slate-25 hover:text-bg-50 flex items-center px-2 rounded-md dark:hover:bg-slate-800 mb-2.5"
       :class="{
         'bg-woot-25 dark:bg-slate-800': isActive,
         'text-ellipsis overflow-hidden whitespace-nowrap max-w-full': shouldTruncate,
